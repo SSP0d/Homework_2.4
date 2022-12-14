09f6e29b4b82f9357ddab76bc3b1a354893d2885
Serhii Pidkopai <spodkopay@gmail.com>
2022-12-14 22:53:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ MESSAGE = "Python Web development"
 
 
 class HttpHandler(BaseHTTPRequestHandler):
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read()) 
+    
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
@@ -33,24 +51,6 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.send_header('Location', '/')
         self.end_headers()
 
-    def send_html_file(self, filename, status=200):
-        self.send_response(status)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        with open(filename, 'rb') as fd:
-            self.wfile.write(fd.read()) 
-
-    def send_static(self):
-        self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
-        if mt:
-            self.send_header("Content-type", mt[0])
-        else:
-            self.send_header("Content-type", 'text/plain')
-        self.end_headers()
-        with open(f'.{self.path}', 'rb') as file:
-            self.wfile.write(file.read())
-
 
 def run_http(server_class=HTTPServer, handler_class=HttpHandler):
     server_address = ('', 3000)
@@ -69,11 +69,10 @@ def run_udp_server(ip, port):
         while True:
             data, address = sock.recvfrom(1024)
             print(f'Received data: {data.decode()} from: {address}')
-
             data_parse = urllib.parse.unquote_plus(data.decode())
             data_dict: dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
             with open("./storage/data.json", "wb") as file:
-                json.dump({datetime.now(): data_dict}, file)
+                json.dump(data_dict, file)
             sock.sendto(data, address)
             print(f'Send data: {data.decode()} to: {address}')
     except KeyboardInterrupt:
